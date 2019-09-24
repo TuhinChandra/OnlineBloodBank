5299c271e5407548bf0d360e0c14b89666193ceb
KITSTuhinChandra <tuhin.chandra>
2019-09-24 21:52:14 +0530

    Removed unnecessary apaces

--- src/main/java/com/online/bloodbank/controller/AdminController.java
+++ src/main/java/com/online/bloodbank/controller/AdminController.java
@@ -28,7 +28,6 @@ public class AdminController {
 
 	// TODO: Use @PostMapping
 	@PostMapping(value = "/admin/login", produces = "application/json")
-
 	@ResponseBody
 	public AdminUsers loginAdmin(@RequestParam("fullName") final String fullName,
 			@RequestParam("password") final String password) {
